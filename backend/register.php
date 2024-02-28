65909abc0de53fb5a973c70d6520495ee2d6957a
NazarT <nt329@njit.edu>
2024-02-28 17:45:46 -0500

    updated frontend/backend

--- backend/register.php
+++ backend/register.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
@@ -6,14 +7,12 @@ require_once('rabbitMQLib.inc');
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-if (isset($_POST['register'])) {
+if (isset($_POST['register']) && (!isset($_SESSION['jwtToken']) && empty($_SESSION['jwtToken']))) {
     $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
     $username = $_POST['username'];
     $firstName = $_POST['firstName'];
     $lastName = $_POST['lastName'];
     $password = $_POST['password'];
-    $browserSession = $_POST['browser'];
-    $lastLogin = $_POST['lastLogin'];
     $password = password_hash($password, PASSWORD_DEFAULT);
     $request = array();
     $request['type'] = "Register";
@@ -21,16 +20,19 @@ if (isset($_POST['register'])) {
     $request['firstName'] = $firstName;
     $request['lastName'] = $lastName;
     $request['password'] = $password;
-    $request['browserSession'] = $browserSession;
-    $request['lastLogin'] = $lastLogin;
     $response = $client->send_request($request);
     echo "client received response: ".PHP_EOL;
     print_r($response);
     echo "\n\n";
-    if($response['returnCode'] == 1){
-        header("Location: landing.html");
-    }else{
-        echo "Not allowed!";
+    if($response['returnCode'] == 1 && $response['message'] == 'Account is registered and connected'){
+        header("Location: index.html");
+        exit();
+    }else if ($response['returnCode'] == 0 && $response['message'] == 'Username already exists') {
+        header("Location: register.html?message=UsernameAlreadyExists");
+        exit();
     }
+}else{
+    header("Location: landing.php");
+    exit();
 }
 ?>
